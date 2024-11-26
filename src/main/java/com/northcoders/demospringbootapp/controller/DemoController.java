9f4f56df1756c5a447bce75a72364330fe76189a
Tawanda Chabva <tchabva@gmail.com>
2024-11-26 11:08:43 +0000

    feat: added @RequestMapping to DemoController
    add the URI path of api/v1 to the class

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,7 +1,10 @@
 package com.northcoders.demospringbootapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class DemoController {
+
 }
