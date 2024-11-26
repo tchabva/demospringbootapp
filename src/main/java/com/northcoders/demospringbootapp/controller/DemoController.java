20f7980d3116ae3d39de904c43278b6c53129a57
Abulaf1a <peterrazzell688@gmail.com>
2024-11-26 11:17:50 +0000

    feat: added getHello method to DemoController class

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,10 +1,18 @@
 package com.northcoders.demospringbootapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
 public class DemoController {
+
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello there!";
+    }
+
 
 }
