0b461abe55dd265ed7426eb4fa11d2829c8dc35e
Tawanda Chabva <tchabva@gmail.com>
2024-11-26 11:44:03 +0000

    feat: created getPeople method in the DemoController class

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,8 +1,11 @@
 package com.northcoders.demospringbootapp.controller;
 
+import com.northcoders.demospringbootapp.model.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -15,4 +18,13 @@ public class DemoController {
     }
 
 
+    List<Person> people = List.of(
+            new Person("Tawanda", 29 ,"Tawanda@testing.com", "Zimbabwe", "Churros"),
+            new Person("Peter", 24, "Peter@testing.com", "England", "Churro-Flavoured Cereal")
+    );
+
+    @GetMapping("/people")
+    public List<Person> getPeople(){
+        return people;
+    }
 }
