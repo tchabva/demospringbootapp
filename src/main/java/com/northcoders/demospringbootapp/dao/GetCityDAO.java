80c5816bc0d7745194d9a4eefd8955ea4cd57cf3
Tawanda Chabva <tchabva@gmail.com>
2024-11-26 15:06:24 +0000

    refactor: added URI method to getCity responseBody variable

--- src/main/java/com/northcoders/demospringbootapp/dao/GetCityDAO.java
+++ src/main/java/com/northcoders/demospringbootapp/dao/GetCityDAO.java
@@ -6,13 +6,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 public class GetCityDAO {
 
-    public static City getCity(){
+    public static City getCity(String city){
         WebClient webClient = WebClient.builder()
-                .baseUrl("https://geocoding-api.open-meteo.com/v1/search?name=London&count=1&language=en&format=json")
+                .baseUrl("https://geocoding-api.open-meteo.com/v1/search")
                 .build();
 
         Results responseBody = webClient
                 .get()
+                .uri("?name="+city+"&count=1&language=en&format=json")
                 .retrieve()
                 .bodyToMono(Results.class)
                 .block();
