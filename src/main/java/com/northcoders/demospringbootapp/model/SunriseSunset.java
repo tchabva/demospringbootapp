cc38be0f8a1248a66dc5b6c2966d122e00fe9d99
Tawanda Chabva <tchabva@gmail.com>
2024-11-27 17:13:25 +0000

    refactor: updated SunriseSunset record toString method

--- src/main/java/com/northcoders/demospringbootapp/model/SunriseSunset.java
+++ src/main/java/com/northcoders/demospringbootapp/model/SunriseSunset.java
@@ -4,4 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SunriseSunset (String sunrise, String sunset){
+
+    @Override
+    public String toString() {
+        return "Sunsrise: " + sunrise + " Sunset: " + sunset;
+    }
 }
